338559ff913c3fc24978f6166d4d46c7eddc33d7
Denis-Copilu <58516918+Denis-Copilu@users.noreply.github.com>
2019-12-09 10:22:53 +0200

    Am adaugat proiectul Unity

---
+++ The wanderer/Assets/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    public float moveSpeed = 3f;
+    public float jumpForce = 40;
+    float velX;
+    float velY;
+    bool facingRight = true;
+   
+    // Update is called once per frame
+    void Update()
+    {
+
+        Jump();
+        velX = Input.GetAxisRaw("Horizontal");
+       // velY = rigBody.velocity.y;
+        //rigBody.velocity = new Vector2(velX * moveSpeed,velY);
+        //rigBody.freezeRotation();
+
+        Vector3 horizontal = new Vector3(velX, 0.0f, 0.0f);
+        transform.position = transform.position + horizontal*moveSpeed;
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+    private void LateUpdate()
+    {
+      Vector3 localScale = transform.localScale;
+        if(velX>0)
+        {
+            facingRight = true;
+
+        }else if(velX<0)
+        {
+            facingRight = false;
+        }
+        if(((facingRight)&&(localScale.x<0))||((!facingRight)&&(localScale.x>0)))
+        {
+            localScale.x *= -1;
+        }
+        transform.localScale = localScale;
+        
+    }
+
+}
---
+++ The wanderer/Assets/terrain.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terrain : MonoBehaviour
+{
+    public const int MIN = 20;
+    public const int MAX = 50;
+    public const int LENGTH = 100;
+    public const int SCAN_RADIUS = 2;
+    public const int SMOOTH_COUNT = 3;
+
+    float[] heightmap = new float[LENGTH];
+    int offset = 0;
+
+    public MeshFilter meshFilter;
+    public Mesh mesh;
+
+    void Awake()
+    {
+        Camera.main.transform.position = new Vector3(LENGTH / 2, LENGTH / 2, -LENGTH);
+
+        mesh = new Mesh();
+        mesh.name = "Terrain mesh";
+        meshFilter.mesh = mesh;
+
+        GenerateHeightmap(offset);
+        BuildMesh();
+    }
+
+    void Update()
+    {
+        int scroll = 0;
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) scroll--;
+        if (Input.GetKeyDown(KeyCode.RightArrow)) scroll++;
+
+        if (scroll != 0)
+        {
+            offset += scroll;
+
+            GenerateHeightmap(offset);
+            BuildMesh();
+        }
+    }
+
+    void GenerateHeightmap(int offset)
+    {
+        UnityEngine.Random.seed = 0;
+        for (int j = 0; j < offset + LENGTH; j++)
+        {
+            float rnd = UnityEngine.Random.Range(MIN, MAX);
+            if (j >= offset)
+            {
+                int i = j - offset;
+                heightmap[i] = rnd;
+            }
+        }
+
+        for (int s = 0; s < SMOOTH_COUNT; s++)
+            Smooth();
+    }
+
+    void Smooth()
+    {
+        for (int i = 0; i < heightmap.Length; i++)
+        {
+            float height = heightmap[i];
+
+            float heightSum = 0;
+            float heightCount = 0;
+
+            for (int n = i - SCAN_RADIUS;
+                     n < i + SCAN_RADIUS + 1;
+                     n++)
+            {
+                if (n >= 0 &&
+                    n < heightmap.Length)
+                {
+                    float heightOfNeighbour = heightmap[n];
+
+                    heightSum += heightOfNeighbour;
+                    heightCount++;
+                }
+            }
+
+            float heightAverage = heightSum / heightCount;
+            heightmap[i] = heightAverage;
+        }
+    }
+
+    void BuildMesh()
+    {
+        mesh.Clear();
+        List<Vector3> positions = new List<Vector3>();
+        List<int> triangles = new List<int>();
+
+        int offset = 0;
+        for (int i = 0; i < LENGTH - 1; i++)
+        {
+            offset = i * 4;
+
+            float h = heightmap[i];
+            float hn = heightmap[i + 1];
+            positions.Add(new Vector3(i + 0, 0, 0)); //lower left - at index 0
+            positions.Add(new Vector3(i + 1, 0, 0)); //lower right - at index 1
+            positions.Add(new Vector3(i + 0, h, 0)); //upper left - at index 2
+            positions.Add(new Vector3(i + 1, hn, 0)); //upper right - at index 3
+
+            triangles.Add(offset + 0);
+            triangles.Add(offset + 2);
+            triangles.Add(offset + 1);
+
+            triangles.Add(offset + 1);
+            triangles.Add(offset + 2);
+            triangles.Add(offset + 3);
+        }
+
+        mesh.vertices = positions.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+    }
+}
